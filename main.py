7de225db3650b9fa63d98f34ec673b6b09491437
David Howlett <davidh@speechmatics.com>
2022-09-26 19:16:40 +0100

    switched data structure to better deduplicate formula fragments with the same totals

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ Performance history:
  I note that the total solve time is entirely dominated by a very small number of tricky problems
  - regenerated the problem set in sorted order, and got a really hard one. I want to keep this problem set long term.
  100 puzzles finished in 385478.2 milliseconds
+ - switched data structure to better deduplicate formula fragments with the same totals
+ 100 puzzles finished in 4953.9 milliseconds
 
 
 """
@@ -37,9 +39,9 @@ def solve(problem):
     # this will eliminate some reasons for the exact same fragment being generated more than once
     for _ in range(len(initial_numbers) - 1):
         previous_main_data_structure = copy.deepcopy(main_data_structure)
-        for used_numbers1, fragment_set1 in previous_main_data_structure.items():
+        for used_numbers1, totals_and_fragments1 in previous_main_data_structure.items():
             # go faster: the below line does twice as many checks as needed.
-            for used_numbers2, fragment_set2 in previous_main_data_structure.items():
+            for used_numbers2, totals_and_fragments2 in previous_main_data_structure.items():
                 # if the two sets of numbers used overlap then generating a valid formula fragment is impossible,
                 # so we should bail out early
                 if used_numbers1 & used_numbers2:
@@ -47,72 +49,38 @@ def solve(problem):
                 # the set of numbers used in the new formula fragment will be the union of the
                 # sets of numbers used in the two formula fragments used to make it up.
                 new_used_numbers = used_numbers1 | used_numbers2
-                place_to_put_new_combos = main_data_structure[new_used_numbers]
-                for total1, formula_fragment1 in fragment_set1:
-                    for total2, formula_fragment2 in fragment_set2:
-                        # go faster: (most important) only add a formula fragment to the main datastructure iff the
-                        # combination of new_used_numbers and new_total is unique. Repeats have no value.
-                        # subtract --------------------------
-                        new_total = total1 - total2
-                        new_thing = (
-                            new_total,
-                            (formula_fragment1, "-", formula_fragment2),
-                        )
-                        if new_total == target:
-                            return new_thing
-                        # go faster: this "if" can be skipped by adding directly
-                        if new_thing not in place_to_put_new_combos:
-                            # print(f"combo {new_thing}")
-                            place_to_put_new_combos.add(new_thing)
-                        # divide ----------------------------
-                        if total2 and not total1 % total2:  # decimals are never useful
-                            new_total = total1 // total2
-                            new_thing = (
-                                new_total,
-                                (formula_fragment1, "/", formula_fragment2),
-                            )
-                            if new_total == target:
-                                return new_thing
-                            # go faster: this "if" can be skipped by adding directly
-                            if new_thing not in place_to_put_new_combos:
-                                # print(f"combo {new_thing}")
-                                place_to_put_new_combos.add(new_thing)
-                        # plus -------------------------------
-                        new_total = total1 + total2
-                        new_thing = (
-                            new_total,
-                            (formula_fragment1, "+", formula_fragment2),
-                        )
-                        new_thing_permutation = (
-                            total1 + total2,
-                            (formula_fragment2, "+", formula_fragment1),
-                        )
-                        if new_total == target:
-                            return new_thing
-                        if (
-                            new_thing not in place_to_put_new_combos
-                            and new_thing_permutation not in place_to_put_new_combos
-                        ):
-                            # print(f"combo {new_thing}")
-                            place_to_put_new_combos.add(new_thing)
-                        # multiply --------------------------
+                new_totals_and_fragments = main_data_structure[new_used_numbers]
+                for total1, formula_fragment1 in totals_and_fragments1.items():
+                    for total2, formula_fragment2 in totals_and_fragments2.items():
+                        # multiply -----------------------------
                         new_total = total1 * total2
-                        new_thing = (
-                            new_total,
-                            (formula_fragment1, "*", formula_fragment2),
-                        )
-                        new_thing_permutation = (
-                            total1 * total2,
-                            (formula_fragment2, "*", formula_fragment1),
-                        )
-                        if new_total == target:
-                            return new_thing
-                        if (
-                            new_thing not in place_to_put_new_combos
-                            and new_thing_permutation not in place_to_put_new_combos
-                        ):
-                            # print(f"combo {new_thing}")
-                            place_to_put_new_combos.add(new_thing)
+                        if new_total not in new_totals_and_fragments:
+                            new_formula_fragment = formula_fragment1, "*", formula_fragment2
+                            new_totals_and_fragments[new_total] = new_formula_fragment
+                            if new_total == target:
+                                return new_formula_fragment
+                        # add ----------------------------------
+                        new_total = total1 + total2
+                        if new_total not in new_totals_and_fragments:
+                            new_formula_fragment = formula_fragment1, "+", formula_fragment2
+                            new_totals_and_fragments[new_total] = new_formula_fragment
+                            if new_total == target:
+                                return new_formula_fragment
+                        # subtract -----------------------------
+                        new_total = total1 - total2
+                        if new_total not in new_totals_and_fragments:
+                            new_formula_fragment = formula_fragment1, "-", formula_fragment2
+                            new_totals_and_fragments[new_total] = new_formula_fragment
+                            if new_total == target:
+                                return new_formula_fragment
+                        # divide -------------------------------
+                        if total2 and not total1 % total2:  # divide by 0 is bad and decimals are never useful
+                            new_total = total1 // total2
+                            if new_total not in new_totals_and_fragments:
+                                new_formula_fragment = formula_fragment1, "/", formula_fragment2
+                                new_totals_and_fragments[new_total] = new_formula_fragment
+                                if new_total == target:
+                                    return new_formula_fragment
     raise ValueError("Problem was impossible, solving failed")
 
 
@@ -135,19 +103,19 @@ def make_main_data_structure(initial_numbers):
     # I want to initialise the main data structure to have every set of possible numbers that could be used in a formula
     # fragment
     main_data_structure = {
-        frozenset(combo): set() for combo in all_combinations_of_initial_numbers
+        frozenset(combo): {} for combo in all_combinations_of_initial_numbers
     }
     # I then add in all the starting numbers to act as the seed for the recursive filling of the main data structure
     # to come. The initial numbers are used to fill up the part of the data structure where the formula
     # would normally be
     for number in initial_numbers_as_strings:
-        main_data_structure[frozenset([number])].add((int(number), int(number)))
+        main_data_structure[frozenset([number])][int(number)] = int(number)
     # print(*(main_data_structure.items()), sep="\n")
     return main_data_structure
 
 
 def solution_to_string(almost_formula):
-    return str(almost_formula[1]).replace("'", "").replace(",", "")
+    return str(almost_formula).replace("'", "").replace(",", "")
 
 
 if __name__ == "__main__":
